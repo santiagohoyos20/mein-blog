a203277945eef07fe876325234fdadc2c1aea84d
Santiago Hoyos <santiagojhoyoss@gmail.com>
2025-07-22 01:50:05 -0500

    fixes

--- src/services/supabaseClient.ts
+++ src/services/supabaseClient.ts
@@ -1,6 +1,5 @@
 import { createClient } from "@supabase/supabase-js"; 
 
-// Hacer .env despues
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 export const supabase = createClient(supabaseUrl, supabaseKey);
